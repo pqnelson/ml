f9125886449730c87e2d18725c905d426fe0af97
Alex Nelson <pqnelson@gmail.com>
2018-11-29 19:07:03 -0800

    [minor] Added exit statuses

--- includes/defs.h
+++ includes/defs.h
@@ -1,10 +1,25 @@
 /**
+ * @file includes/defs.h
+ * 
  * System-independent definitions.
  *
  * We need to guarantee that booleans, etc., are well-defined even for
  * pre-C99 compilers. And here it is!
  *
  * Don't expose this to the children...
+ * 
+ * @section sec Exit Codes
+ *
+ * We have "standardized" exit codes, depending on what errors occur.
+ * (Success is always <code>EXIT_SUCCESS</code>, quite boring.)
+ *
+ * The codes are compliant with the @c <sysexits.h> header file, we just
+ * specify the errors more explicitly. For example, instead of
+ * @c EX_OSERR for when @c malloc() returns a null pointer, we simply
+ * define @c EXIT_MALLOCERR for that error (whose value coincides with
+ * @c EX_OSERR since such a failure is a specific instance of an OS error).
+ *
+ * @see https://www.freebsd.org/cgi/man.cgi?query=sysexits
  * 
  * @author Alex Nelson <pqnelson@gmail.com>
  */
@@ -14,13 +29,16 @@
 /**
  * Print to the error stream.
  *
- * To print the errors to somewhere else, simply use <code>freopen()</code>
- * to use a different file for <code>stderr</code>.
- * 
- * @ref https://gcc.gnu.org/onlinedocs/gcc-4.4.7/cpp/Variadic-Macros.html
+ * The same as @c printf but specifically to the @c stderr output stream.
+ *
+ * @TODO Make this print always to the output stream with file descriptor 2?
+ * @see https://gcc.gnu.org/onlinedocs/gcc-4.4.7/cpp/Variadic-Macros.html
  */
 #define eprintf(...) fprintf (stderr, __VA_ARGS__)
 
+/**
+ * @cond DEV
+ */
 /* {@see https://stackoverflow.com/a/2115886} */
 #if defined(__STDC__)
 # define C89
@@ -35,13 +53,22 @@
 #  endif /* (__STDC_VERSION__ >= 199901L) */
 # endif /* defined(__STDC_VERSION__) */
 #endif /* defined(__STDC__) */
+/**
+ * @endcond
+ */
 
 /**
  * Boolean types are well-defined.
+ *
+ * For C99 compliant compilers, this defaults to including the @c <stdbool.h>
+ * header. We implement an @c enum version for pre-C99 compilers.
  */
 #ifdef C99
 #include <stdbool.h>
 #else
+/**
+ * This definition of @c bool is taken from Donald Knuth.
+ */
 typedef enum
 {
 	false = (1 == 0),
@@ -51,7 +78,27 @@ typedef enum
 
 /**
  * For use in computing the hash code of objects.
+ *
+ * I know that having a <code>_t</code> suffix is "bad form", but still...
  */
 typedef long hash_t;
+
+/**
+ * File reading/writing error status.
+ */
+#define EXIT_IOERR 74
+/**
+ * Malloc returns @c NULL pointer.
+ */
+#define EXIT_MALLOCERR 71
+/**
+ * Handle bad input from user.
+ *
+ * Example: runaway comments, malformed input, etc.
+ *
+ * This coincides with @c <sysexits.h> value for @c EX_DATAERR.
+ */
+#define EXIT_BAD_INPUT 65
+
 
 #endif /* DEFS_H */
