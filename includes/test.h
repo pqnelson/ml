869a94275c1bd81404012446a219b26e2e9cfa80
Alex Nelson <pqnelson@gmail.com>
2018-12-04 17:04:57 -0800

    assertEquals() now c99 compliant

--- includes/test.h
+++ includes/test.h
@@ -111,7 +111,7 @@ void TestCase_printXML(TestCase *test, FILE *xml);
         this->message = (msg);                                       \
     }
 
-#define assertEqualHelper(lhs, rhs, compare, msg, ...)   \
+#define assertEqualHelper(lhs, rhs, compare, msg)        \
     if (TestCase_isRunnable(this)) {                     \
         if (compare((lhs), (rhs))) {                     \
              TestCase_markSuccess();                     \
@@ -124,7 +124,7 @@ void TestCase_printXML(TestCase *test, FILE *xml);
 #define str(x) xstr(x)
 // The empty variadic arguments consumes a third argument, which isn't
 // needed anyways, hence the underscore argument.
-#define _assertEqual2(lhs, rhs, _)                                         \
+#define _assertEqual2(lhs, rhs)                                            \
     if (TestCase_isRunnable(this)) {                                       \
         if ((lhs) == (rhs)) {                                              \
              TestCase_markSuccess();                                       \
@@ -156,9 +156,27 @@ void TestCase_printXML(TestCase *test, FILE *xml);
             printf("%s FAIL: %s\n", this->name, str(cond) " is TRUE, expected FALSE");   \
         }                                                                 \
     } else { }
+// corrected macros adopted from https://groups.google.com/forum/#!topic/comp.std.c/d-6Mj5Lko_s
+#define PP_NARG_(...) \
+         PP_ARG_N(__VA_ARGS__)
+#define PP_ARG_N( \
+          _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+         _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+         _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+         _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+         _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+         _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+         _61,_62,_63,N,...) N
+#define PP_RSEQ_N() \
+         63,62,61,60,                   \
+         59,58,57,56,55,54,53,52,51,50, \
+         49,48,47,46,45,44,43,42,41,40, \
+         39,38,37,36,35,34,33,32,31,30, \
+         29,28,27,26,25,24,23,22,21,20, \
+         19,18,17,16,15,14,13,12,11,10, \
+         9,8,7,6,5,4,3,2,1,0
 
-#define _GET_NTH_ARG(_3, _2, _1, N, ...) N
-#define COUNT_VARARGS(...) _GET_NTH_ARG(__VAR_ARGS__, 4, 3, 2)
+#define COUNT_VARARGS(...) PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
 #define _concat(x, y) x ## y
 #define concat(x, y) _concat(x, y)
 /** @endcond */
@@ -176,7 +194,7 @@ void TestCase_printXML(TestCase *test, FILE *xml);
  *        @li <span class="paramname">msg</span> User-provided message shown upon failure, defaults to
  *        <code>testCase->name " FAIL: " str(lhs) != str(rhs)</code>.
  */
-#define assertEqual(lhs, rhs, ...) concat(_assertEqual, COUNT_VARARGS(__VA_ARGS__)) (lhs, rhs, __VA_ARGS__)
+#define assertEqual(...) concat(_assertEqual, COUNT_VARARGS(__VA_ARGS__)) (__VA_ARGS__)
 
 /**
  * A composite of @c TestCase objects.
